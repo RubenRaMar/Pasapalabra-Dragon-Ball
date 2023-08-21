37207e403659866b4f38fcec4e88d8137cf034a6
RubenRaMar <rubenfr1@hotmail.com>
2023-08-21 12:19:11 +0200

    Refactor geneateranking module

--- js/generateranking.js
+++ js/generateranking.js
@@ -6,29 +6,23 @@ class GetUserData {
   }
 }
 
-const generateRanking = (gameData) => {
+const generateRanking = ({ username, answerPoints, usersPoints }) => {
   let rankingPosition = 0;
 
-  new GetUserData(
-    gameData.username,
-    gameData.answerPoints,
-    gameData.usersPoints
-  );
+  new GetUserData(username, answerPoints, usersPoints);
 
-  gameData.usersPoints.sort((a, b) => b.answerPoints - a.answerPoints);
+  usersPoints.sort((a, b) => b.answerPoints - a.answerPoints);
 
   rankingRows.forEach((row) => {
-    if (rankingPosition <= gameData.usersPoints.length - 1) {
+    if (rankingPosition <= usersPoints.length - 1) {
       const rowChildrens = row.children;
 
       for (let i = 0; i < rowChildrens.length; i++) {
         const children = rowChildrens[i];
         if (i === 0) children.innerHTML = rankingPosition + 1;
         if (i === 1)
-          children.innerHTML =
-            gameData.usersPoints[rankingPosition].answerPoints;
-        if (i === 2)
-          children.innerHTML = gameData.usersPoints[rankingPosition].username;
+          children.innerHTML = usersPoints[rankingPosition].answerPoints;
+        if (i === 2) children.innerHTML = usersPoints[rankingPosition].username;
       }
       rankingPosition++;
     }
